4d45f01067ceb64190989573f9a6c1b5eca3a750
Alvin Lander <alvin.lander@miraeasset.co.id>
2023-07-26 08:07:34 +0700

    change treshold redirect

--- resources/views/admin/detail.blade.php
+++ resources/views/admin/detail.blade.php
@@ -97,7 +97,7 @@
 <script>
     setTimeout(function(){
     window.location.replace('/admin/scan');
-    }, 5000);
+    }, 2000);
 </script>
 @endif
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
